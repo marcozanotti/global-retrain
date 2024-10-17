48be237365f2aadfae982101ef0edc8a21c3f5d0
Marco Zanotti <zanottimarco17@gmail.com>
2024-10-17 17:03:47 +0200

    initial setup

---
+++ src/Python/download_data.py
@@ -0,0 +1,14 @@
+# Collect datasets
+
+# download M3, M4, M5
+
+from datasetsforecast.m3 import M3
+from datasetsforecast.m4 import M4
+from datasetsforecast.m5 import M5
+
+
+M3.download('data')
+M4.download('data') 
+M5.download('data')
+
+
