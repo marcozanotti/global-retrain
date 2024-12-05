8af2901d3210ef4e6e512cd95b4f2af2c2647094
Marco Zanotti <zanottimarco17@gmail.com>
2024-12-05 12:12:03 +0100

    add colored logs con console logger

--- src/Python/utils/utilities.py
+++ src/Python/utils/utilities.py
@@ -11,6 +11,12 @@ def configure_logging(log_file):
         log_file (str): Name of the log file.
     """
 
+    grey = '\x1b[38;20m'
+    yellow = '\x1b[33;20m'
+    red = '\x1b[31;20m'
+    bold_red = '\x1b[31;1m'
+    reset = '\x1b[0m'
+
     logging.config.dictConfig({ # Centralized logging configuration using dictConfig
         'version': 1, # Configuration schema version
         'disable_existing_loggers': False, # Ensure existing loggers are not disabled
@@ -22,12 +28,15 @@ def configure_logging(log_file):
             },
             'console': {
                 'class': 'logging.StreamHandler',  
-                'formatter':'simple'  
+                'formatter':'colored'  
             }
         },
         'formatters': { # Formatters define the structure of the log messages
             'simple': {
                 'format': '%(asctime)s : %(name)s : %(levelname)s : %(message)s'
+            },
+            'colored': {
+                'format': yellow + '%(asctime)s : %(name)s : %(levelname)s : %(message)s' + reset
             }
         },
         'root': { # The root logger configuration
