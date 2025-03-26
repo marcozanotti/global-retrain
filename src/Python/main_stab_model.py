ec83383d8ec9d3098c80087842bf1cad4a277a56
mz21108 <m.zanotti22@campus.unimib.it>
2025-03-26 17:06:53 +0100

    test model stability

--- src/Python/main_stab_model.py
+++ src/Python/main_stab_model.py
@@ -8,8 +8,8 @@ from utilities import (
 from evaluate_stability import evaluate_model_stability
 
 os.environ['NIXTLA_ID_AS_COL'] = '1'
-config = get_config('config/stab/stab_vn1_weekly_config.yaml')
-# config = get_config('config/stab/stab_m5_daily_config.yaml')
+# config = get_config('config/stab/stab_vn1_weekly_config.yaml')
+config = get_config('config/stab/stab_m5_daily_config.yaml')
 configure_logging(
     config_file = 'config/log_config.yaml', 
     name_list = [
