8e96014d9df4bad47e3f9b5dbb73ac366c712337
mz21108 <m.zanotti22@campus.unimib.it>
2025-03-27 14:31:50 +0100

    update ens config

--- src/Python/main_ensemble.py
+++ src/Python/main_ensemble.py
@@ -1,15 +1,17 @@
+import sys
+sys.path.insert(0, 'src/Python/utils')
 import os
-from src.Python.utils.utilities import (
+from utilities import (
     get_config, configure_logging, create_logger, stop_logger
 )
-from src.Python.utils.fit_ensembles import fit_ensembles
+from fit_ensembles import fit_ensembles
 
 os.environ['NIXTLA_ID_AS_COL'] = '1'
 # config = get_config('config/ensemble/ensemble_vn1_monthly_config.yaml')
 # config = get_config('config/ensemble/ensemble_vn1_weekly_config.yaml')
 # config = get_config('config/ensemble/ensemble_m5_monthly_config.yaml')
-config = get_config('config/ensemble/ensemble_m5_weekly_config.yaml')
-# config = get_config('config/ensemble/ensemble_m5_daily_config.yaml')
+# config = get_config('config/ensemble/ensemble_m5_weekly_config.yaml')
+config = get_config('config/ensemble/ensemble_m5_daily_config.yaml')
 configure_logging(
     config_file = 'config/log_config.yaml', 
     name_list = [
