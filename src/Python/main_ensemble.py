0a3ec7e48d3af807a3ad72236beb5bb81fb1b313
mz21108 <m.zanotti22@campus.unimib.it>
2025-07-27 21:28:24 +0200

    sis2026 config

--- src/Python/main_ensemble.py
+++ src/Python/main_ensemble.py
@@ -11,7 +11,7 @@ os.environ['NIXTLA_ID_AS_COL'] = '1'
 # config = get_config('config/ensemble/ensemble_vn1_weekly_config.yaml')
 # config = get_config('config/ensemble/ensemble_m5_monthly_config.yaml')
 # config = get_config('config/ensemble/ensemble_m5_weekly_config.yaml')
-config = get_config('config/ensemble/ensemble_m5_daily_config.yaml')
+config = get_config('config/ensemble/ensemble_m4_daily_config.yaml')
 configure_logging(
     config_file = 'config/log_config.yaml', 
     name_list = [
