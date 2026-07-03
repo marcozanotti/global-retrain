d400ad690a7f473f56ed7c9be77ce0f02d255f00
Marco Zanotti <zanottimarco17@gmail.com>
2026-07-03 18:22:18 +0200

    add sample to break detection

--- src/Python/breaks_detection.py
+++ src/Python/breaks_detection.py
@@ -19,6 +19,15 @@ nobs = 52*5
 res = get_breaks(df=data, nobs=nobs, max_breaks=10, selection='bic', exog=None)
 stop_logger(logger)
 
+# add sample information
+test_window = 104
+sample_df = data[data['ds'] >= (data['ds'].max() - pd.DateOffset(weeks=(test_window-1)))][['ds']] \
+    .drop_duplicates() \
+    .reset_index(drop=True)
+sample_df['sample'] = sample_df.index.astype(int)
+res = res.merge(sample_df, on='ds', how='left')
+res['sample'] = res['sample'].fillna(-1).astype(int)
+
 data_br = data.merge(res, on=['unique_id', 'ds'], how='left')
 data_br['break'] = data_br['break'].fillna('no break')
 save_data(
@@ -27,7 +36,7 @@ save_data(
     name_list=[f'hapag_region_weekly_breaks_full_{nobs}']
 )
 
-data_br_light = res[res['break'] == 'break'][['unique_id', 'ds']]
+data_br_light = res[res['break'] == 'break'][['unique_id', 'ds', 'sample']]
 data_br_light.reset_index(drop=True, inplace=True)
 save_data(
     data_br_light, 
